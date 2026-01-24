a119d6f496d0ddb68cd83e8a82662abb819f7ee5
oovaa <omarvx211@gmail.com>
2026-01-24 12:55:47 +0200

    updated the options to match the consumer at 2026-01-24

--- src/rmq.conf.ts
+++ src/rmq.conf.ts
@@ -4,10 +4,8 @@ export function getRmqConfig(queue = 'iot'): RmqOptions {
   return {
     transport: Transport.RMQ,
     options: {
-      urls: [
-        process.env.RABBITMQ_URL || 'amqp://iot:iot@142.93.134.175:5672',
-      ],
-      queue: queue,
+      urls: ['amqp://iot:iot@142.93.134.175:5672?heartbeat=60'],
+      queue: 'iot', 
       queueOptions: {
         durable: false,
       },
